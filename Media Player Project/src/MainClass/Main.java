318c10aea65bac888931b4ff6b8dad3486235b71
astewart2000 <39848845+astewart2000@users.noreply.github.com>
2018-06-11 11:44:01 -0400

    1.3

--- Media Player Project/src/MainClass/Main.java
+++ Media Player Project/src/MainClass/Main.java
@@ -1,10 +1,14 @@
 package MainClass;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
 import java.io.IOException;
 import java.util.Objects;
 
@@ -24,6 +28,10 @@ public class Main extends Application {
             stage.setScene(new Scene(root));
             stage.setTitle("Media Player");
             stage.show();
+            stage.setOnCloseRequest(t -> {
+                Platform.exit();
+                System.exit(0);
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
