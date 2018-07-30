0e6d15bb0f80dce75a7c344a1aafc887c267f699
Aidan Stewart <39848845+astewart2000@users.noreply.github.com>
2018-07-30 13:51:55 -0400

    v.1.3.4
    File explorer in application fix.

--- Media Player Project/src/MainClass/Main.java
+++ Media Player Project/src/MainClass/Main.java
@@ -2,10 +2,13 @@ package MainClass;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
 import java.io.IOException;
 import java.util.Objects;
 
